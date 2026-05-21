6a902acf8850035f0a4a7eb9d59c982570f91ef8
choijongtack <jongtack63@gmail.com>
2026-05-21 22:27:56 +0900

    commit

---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'jt-bible-cache-v2';
+const BASE_URL = new URL(self.registration.scope).pathname;
+const urlsToCache = [
+  BASE_URL,
+  `${BASE_URL}index.html`
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.ok && !event.request.url.includes('aistudiocdn.com')) {
+            cache.put(event.request, networkResponse.clone());
+          }
+
+          return networkResponse;
+        });
+      })
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
